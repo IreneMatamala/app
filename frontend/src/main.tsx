2ded217ef9b3450017f54e7d743edf0b48cff65f
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-11-24 12:49:14 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
+// Este es el PUNTO DE ENTRADA de tu aplicación React
+// Es el primer archivo que se ejecuta cuando alguien visita tu web
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
